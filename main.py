3500815330f30f317ce8984563c33d9299d91969
Andreas Calleja <andreascalleja@gmail.com>
2022-10-28 23:18:27 +0200

    Working on annotations

--- main.py
+++ main.py
@@ -38,16 +38,20 @@ def get_xg(data):
 		'h_min': [0],
 		'a_min': [0],
 		'h_team': data_home[0]['h_team'],
-		'a_team': data_away[0]['a_team']
+		'a_team': data_away[0]['a_team'],
+		'h_result': ['start'],
+		'a_result': ['start']
 	}
 
 	for shot_event in data_home:
 		xg_data['h_xg'].append(float(shot_event['xG']))
 		xg_data['h_min'].append(int(shot_event['minute']))
+		xg_data['h_result'].append(shot_event['result'])
 
 	for shot_event in data_away:
 		xg_data['a_xg'].append(float(shot_event['xG']))
 		xg_data['a_min'].append(int(shot_event['minute']))
+		xg_data['a_result'].append(shot_event['result'])
 
 	xg_data['h_cumulative'] = nums_cumulative_sum(xg_data['h_xg'])
 	xg_data['a_cumulative'] = nums_cumulative_sum(xg_data['a_xg'])
@@ -62,8 +66,8 @@ def get_xg(data):
 
 def create_flowchart(xg_data):
 	fig, ax = plt.subplots(figsize=(10,5))
-	fig.set_facecolor('#3D4849')
-	ax.patch.set_facecolor('#3D4849')
+	fig.set_facecolor('#4F646F')
+	ax.patch.set_facecolor('#4F646F')
 	plt.xticks([15, 30, 45, 60, 75, 90])
 	plt.xlabel('Minute')
 	plt.xlabel('xG')
@@ -75,12 +79,19 @@ def create_flowchart(xg_data):
 	ax.step(x=xg_data['a_min'], y=xg_data['a_cumulative'], label=xg_data['a_team'], linewidth=3)
 	legend = plt.legend(loc="lower right")
 	frame = legend.get_frame()
-	frame.set_facecolor('#3D4849')
-	frame.set_edgecolor('#3D4849')
+	frame.set_facecolor('#4F646F')
+	frame.set_edgecolor('#4F646F')
 	
-	fig.text(0.13, 0.96, 'xG Flowchart', style = 'italic', fontsize = 15, color = "#FFFFFF")
+	fig.text(0.13, 0.96, 'xG Flowchart', style = 'italic', fontsize = 15, color = "#B7ADCF")
 	teams = f"{xg_data['h_team']} xG {h_xg_score} - xG {a_xg_score} {xg_data['a_team']}"
-	fig.text(0.13, 0.9, teams, style = 'oblique', fontsize = 15, color = "#FFFFFF")
+	fig.text(0.13, 0.9, teams, style = 'oblique', fontsize = 15, color = "#B7ADCF")
+
+	for count, value in enumerate(xg_data['h_result']):
+		if value == 'Goal':
+			print('GOAL')
+			print(xg_data['h_xg'][count])
+			print(xg_data['h_min'][count])
+    	# plt.annotate(y1_plot[i], (x1[i], y1[i]),c='black',size=12,ha='center',va='center',fontweight='bold')
 
 	ax.set_ylim(ymin=0)
 	ax.set_xlim(xmin=0, xmax=90)
